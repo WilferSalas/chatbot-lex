612b1a37b823dd13067fa46f1006656db37c11c7
TEAM\Wilfer.Salas <Wilfer Salas>
2021-03-25 19:02:14 -0500

    Add Dockerfile

---
+++ build/precache-manifest.c72ca0df094ae681b336c4ab9e895e7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf5720c8c62a85fa93c64c68cc2c1d42",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00fda92a7e1c1f8f4a1b",
+    "url": "/static/js/2.cf1ebdd8.chunk.js"
+  },
+  {
+    "revision": "d8e6b9b6d6dc198439d65c8fa1b66951",
+    "url": "/static/js/2.cf1ebdd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "427be78b4ddad7bc6db4",
+    "url": "/static/js/main.6b7d203e.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
