5855d29c0f925f2ead298c067a81ae8f9ba97a5f
TEAM\Wilfer.Salas <Wilfer Salas>
2021-03-27 08:08:30 -0500

    Resolve test error

--- cypress/integration/chat.spec.js
+++ cypress/integration/chat.spec.js
@@ -1,11 +1,11 @@
-<reference types="cypress" />
+/// <reference types="cypress" />
 
 describe('visit Chatea con Lex', () => {
   // beforeEach(() => {
   //   cy.visit('http://localhost:3000/');
   // });
 
-  describe('when user type and send a message', () => {
+  // describe('when user type and send a message', () => {
     it('then the message should be visible on the screen', () => {
       // // Arrange
       // cy.get('[data-automation="chat-input"]').type('Hola');
@@ -16,8 +16,8 @@ describe('visit Chatea con Lex', () => {
       // // Assert
       // cy.get('[data-automation="messages"]').should('have.length', 1)
       //   .then(($lis) => {
-      //     expect($lis).to.have.length(1)
+      //     expect($lis).to.have.length(1) // true
       // });
     });
-  });
+  // });
 });
